415e7eb5555830278933d6cef120efcf775e1087
lisekilis <101191226+lisekilis@users.noreply.github.com>
2024-06-21 19:01:52 +0200

    ~(>_<。)＼

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,30 @@ import { cfg } from "./config";
 const inject = new Injector();
 const logger = Logger.plugin("Replugged-FxTwitter");
 function fixup(content: string): string {
-  const twitterIndex = content.indexOf("https://twitter.com");
-  if (twitterIndex !== -1) {
-    content = `${content.slice(0, twitterIndex)}https://fxtwitter.com${content.slice(twitterIndex, content.length)}`;
+  logger.log(content);
+  const twitterRegex =
+    /(https:\/\/)(www\.)?(?:twitter\.com\/|twitter\.com\/@)([a-zA-Z0-9_]+)\/status\/([0-9]{19})\??/i;
+  const twitter = twitterRegex.exec(content);
+  if (twitter) {
+    if (content[twitter.index - 1] == "\\") {
+      return content.slice(0, twitter.index - 2) + content.slice(twitter.index, content.length);
+    }
+    logger.log(twitter);
+    const twitterIndex = twitter.index + twitter[1].length + (twitter[2] ? twitter[2].length : 0);
+    content = `${content.slice(0, twitterIndex)}fx${content.slice(twitterIndex)}`;
     logger.log("fixed up twitter!");
     return fixup(content);
   }
-  const xIndex = content.indexOf("https://x.com");
-  if (xIndex !== -1) {
-    content = `${content.slice(0, xIndex)}https://fixupx.com${content.slice(xIndex, content.length)}`;
+  const xRegex =
+    /(https:\/\/)(www\.)?(?:x\.com\/|x\.com\/@)([a-zA-Z0-9_]+)\/status\/([0-9]{19})\??/i;
+  const x = xRegex.exec(content);
+  if (x) {
+    if (content[x.index - 1] == "\\") {
+      return content.slice(0, x.index - 2) + content.slice(x.index, content.length);
+    }
+    logger.log(x);
+    const xIndex = x.index + x[1].length + (x[2] ? x[2].length : 0);
+    content = `${content.slice(0, xIndex)}fixup${content.slice(xIndex)}`;
     logger.log("fixed up x!");
     return fixup(content);
   }
